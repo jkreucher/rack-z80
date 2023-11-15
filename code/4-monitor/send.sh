e5f1160b07694e03395531f0ceeb9aad01d1149a
Jannik Kreucher <jannik.kreucher@th-bingen.de>
2023-11-15 16:15:12 +0100

    monitor send bytes script

--- code/4-monitor/send.sh
+++ code/4-monitor/send.sh
@@ -3,6 +3,7 @@ if [ -n "$1" ]
 then
     #stty -F /dev/ttyUSB0 9600 cs8 -parenb -cstopb -ixon
     stty -F /dev/ttyUSB0 9600 litout -crtscts
+    printf "load\x0d" > /dev/ttyUSB0
     xxd -p $1 | tr -d '\n' > /dev/ttyUSB0
     printf "\x0d" > /dev/ttyUSB0
 else
